2770b35c1c05414ab96e154fb70b20a302e63409
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-08-27 18:27:45 +0200

    removed comments

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const darkMode = document.querySelector(".logo #darkmode");
 
 darkMode.addEventListener("click", function(){
     const body = document.querySelector("body");
-    //const navLinks = document.querySelectorAll("a");
 
     if(body.style.backgroundColor === 'black'){
         body.style.backgroundColor = 'beige';
         body.style.color = 'black';
-        //navLinks.forEach(link => link.style.color = "black");
         
     }
     else{
         body.style.backgroundColor = 'black';
         body.style.color = 'white';
-        //navLinks.forEach(link => link.style.color = "white");
     }
 })
