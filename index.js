b4576b4e688ea4353f849e1960f286e8fe0813d0
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-09-02 14:20:30 +0200

    removed dark mode to make things simple

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const darkMode = document.querySelector(".logo #darkmode");
-
-darkMode.addEventListener("click", function(){
-    const body = document.querySelector("body");
-
-    if(body.style.backgroundColor === 'black'){
-        body.style.backgroundColor = 'white';
-        body.style.color = 'black';
-        
-    }
-    else{
-        body.style.backgroundColor = 'black';
-        body.style.color = 'white';
-    }
-})
 
 // photo shuffle
 const PhotoShuffleDiv = document.querySelector(".photoshuffle");
