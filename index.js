d8eb3e8f280efde7d45d3e9823ed57c6390ffa16
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-09-05 16:34:00 +0200

    finished the playlist and description

--- index.js
+++ index.js
@@ -82,8 +82,129 @@ let interval = setInterval(()=>{
 
 //code for the javascript code for the songs and playlist
 let SongPlaylist = document.querySelector(".songplaylist");
+const Audio = document.querySelector("#myAudio");
+
+//the artist variables
+let Asake = SongPlaylist.querySelector("#asake");
+let DojaCat = SongPlaylist.querySelector("#dojacat");
+let PartyNDoor = SongPlaylist.querySelector("#partynextdoor");
+let Sujal = SongPlaylist.querySelector("#sujal");
+let Winny = SongPlaylist.querySelector("#winny");
+let PostMalone = SongPlaylist.querySelector("#postmalone");
+
+//music source
+const MusicSource = document.querySelector("#source");
+
+//artis image
+let ArtistImage = document.querySelector(".artist_image")
+
+//artist name
+let SelectedArtistName = document.querySelector(".artist_name");
+let Title = document.querySelector(".title");
+
+//description text
+let DescriptionText = document.querySelector(".description_text");
+let Year = document.querySelector(".year");
 
 
+//time for eventlisteners
+Asake.addEventListener("click",function(){
+   MusicSource.setAttribute("src","/music/Asake, Wizkid - MMS (Official Visualizer)(1).mp3")
+   Audio.load();
+   Audio.play();
+
+   //changing the image
+   ArtistImage.src = "/static/images/asake.png";
+
+   //changing the details
+   SelectedArtistName.innerHTML = "Asake X Wizkid";
+   Title.innerHTML = "MMS";
+   DescriptionText.innerHTML = "For this incredible soundtrack, Asake teamed up with<br> the legendary Afrobeats sensation, Wizkid, whose influence adds<br> a dynamic layer to the song’s<br> production";
+   Year.innerHTML = "2024";
+
+})
+
+DojaCat.addEventListener("click",function(){
+   MusicSource.setAttribute("src","/music/Doja Cat - So High (Official Audio).mp3")
+   Audio.load();
+   Audio.play();
+
+   //changing the image
+   ArtistImage.src = "/static/images/dojacat.png";
+
+   //changing the details
+   SelectedArtistName.innerHTML = "Doja Cat";
+   Title.innerHTML = "So High";
+   DescriptionText.innerHTML = "So High is the debut single by American rapper<br> and singer Doja Cat. She originally self-published an early version of the song<br> exclusively to SoundCloud on November 8";
+   Year.innerHTML = "2012";
+
+})
+
+PartyNDoor.addEventListener("click",function(){
+   MusicSource.setAttribute("src","/music/PARTYNEXTDOOR - FOR CERTAIN.mp3")
+   Audio.load();
+   Audio.play();
+
+   //changing the image
+   ArtistImage.src = "/static/images/partynextdoor.png";
+
+   //changing the details
+   SelectedArtistName.innerHTML = "PartyNextDoor";
+   Title.innerHTML = "For Certain";
+   DescriptionText.innerHTML = "Jahron Anthony PartyNextDoor was born to a Jamaican mother<br> and a Trinidadian father in Mississauga, Ontario.";
+   Year.innerHTML = "2024";
+
+})
+
+PostMalone.addEventListener("click",function(){
+   MusicSource.setAttribute("src","/music/Post Malone - Laugh It Off (Official Lyric Video)(1).mp3")
+   Audio.load();
+   Audio.play();
+
+   //changing the image
+   ArtistImage.src = "/static/images/postmalone.png";
+
+   //changing the details
+   SelectedArtistName.innerHTML = "Post Malone";
+   Title.innerHTML = "Laugh It Off";
+   DescriptionText.innerHTML = "Austin Richard Post, known professionally as Post Malone, is an American rapper,<br> singer, songwriter and record producer..";
+   Year.innerHTML = "2023";
+
+})
+
+
+Sujal.addEventListener("click",function(){
+   MusicSource.setAttribute("src","/music/Rushy - Pressure [Music Video].mp3")
+   Audio.load();
+   Audio.play();
+
+   //changing the image
+   ArtistImage.src = "/static/images/sujal.png";
+
+   //changing the details
+   SelectedArtistName.innerHTML = "Rushy";
+   Title.innerHTML = "Pressure";
+   DescriptionText.innerHTML = "Recording “Trippidy Trap” in 2017, Rushy is now coming into himself as an artist<br> and understanding what pockets he fills, I’m<br> in the studio with him a lot and he gets tighter<br> and tighter with every session.";
+   Year.innerHTML = "2023";
+
+})
+
+
+Winny.addEventListener("click",function(){
+   MusicSource.setAttribute("src","/music/Winny - Pretty (Short Film) [Lyric Video].mp3")
+   Audio.load();
+   Audio.play();
+
+   //changing the image
+   ArtistImage.src = "/static/images/winny.png";
+
+   //changing the details
+   SelectedArtistName.innerHTML = "Winny";
+   Title.innerHTML = "Pretty";
+   DescriptionText.innerHTML = "With a beautifully distinct vocal tone and texture,<br> the impressive young artist seeks to communicate compassion,<br> self awareness and belonging in her music";
+   Year.innerHTML = "2022";
+
+})
 
 
 
